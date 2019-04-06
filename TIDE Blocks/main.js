723fa8d9143a042c84c22223df2e210a0b2b25fb
Andrés Flores <aflores2015@alu.uct.cl>
2019-04-06 16:13:38 -0400

    Distribution
    Aplicación empaquetada en windows x64

--- TIDE Blocks/main.js
+++ TIDE Blocks/main.js
@@ -5,74 +5,39 @@ const fs = require('fs');
 
 const {app, BrowserWindow, Menu, ipcMain} = electron;
 
-
 let mainWindow;
-let openDialog;
 
-/* Listen for app to be ready */
-app.on('ready', function () {
-	/* Create new window */
+function createMainWindow () {
 	mainWindow = new BrowserWindow({
 		minWidth: 800,
 		minHeight: 600,
 		frame: false
 	});
-	/* Load html into window */
 	mainWindow.loadURL(url.format({
 		pathname: path.join(__dirname, 'home.html'),
 		protocol: 'file:',
 		slashes: true
 	}));
-	/* Quit app when closed*/
 	mainWindow.on('closed', function() {
 		app.quit();
 	});
-	/* Remove menu bar */
-	//Menu.setApplicationMenu(null);
-});
-
-/* Handle create open dialog */
-function createOpenDialog () {
-	/* Create new window */
-	openDialog = new BrowserWindow({
-		width: 400,
-		height: 300,
-		minWidth: 400,
-		minHeight: 300,
-		maxWidth: 400,
-		maxHeight: 300,
-		minimizable: false,
-		maximizable: false,
-		frame: false
-	});
-	/* Load html into window */
-	openDialog.loadURL(url.format({
-		pathname: path.join(__dirname, 'open.html'),
-		protocol: 'file:',
-		slashes: true
-	}));
 }
 
-/* Listen for app events */
-ipcMain.on('nav:mini', function(event) {
+function navMini (event) {
 	BrowserWindow.getFocusedWindow().minimize();
-});
+}
 
-ipcMain.on('nav:maxi', function(event) {
+function navMaxi (event) {
 	if (BrowserWindow.getFocusedWindow().isMaximized()) {
 		BrowserWindow.getFocusedWindow().restore();
 	} else {
 		BrowserWindow.getFocusedWindow().maximize();
 	}
-});
+}
 
-ipcMain.on('nav:exit', function(event) {
+function navExit (event) {
 	BrowserWindow.getFocusedWindow().close();
-});
-
-ipcMain.on('file:open', function(event) {
-	createOpenDialog();
-});
+}
 
 /*
 ipcMain.on('file:open', function(event, dirname) {
@@ -85,3 +50,8 @@ ipcMain.on('file:open', function(event, dirname) {
 	});
 });
 */
+
+ipcMain.on('nav:mini', navMini);
+ipcMain.on('nav:maxi', navMaxi);
+ipcMain.on('nav:exit', navExit);
+app.on('ready', createMainWindow);
